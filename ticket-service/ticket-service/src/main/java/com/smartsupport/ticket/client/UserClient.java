20a016a49ab0005f744cbb448177892f78a2667a
shahbaz786929 <shahbaz786929@gmail.com>
2026-06-16 18:13:00 +0530

    fixed auth service url

--- ticket-service/ticket-service/src/main/java/com/smartsupport/ticket/client/UserClient.java
+++ ticket-service/ticket-service/src/main/java/com/smartsupport/ticket/client/UserClient.java
@@ -3,7 +3,10 @@ package com.smartsupport.ticket.client;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@FeignClient(name = "AUTH-SERVICE")
+@FeignClient(
+        name = "AUTH-SERVICE",
+        url = "${auth.service.url}"
+)
 public interface UserClient {
 
     @GetMapping("/api/admin/users/count")
